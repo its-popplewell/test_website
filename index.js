114987c73c8dc651fbfd077814fd1c9352dc22bd
its-popplewell <jpscheffel@gmail.com>
2020-11-13 14:54:57 -0500

    Add one off Event Listener

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 console.log("Hello World!");
 
-document.body.addEventListener("keypress", (event) => {
-    console.log("SPACES");
-});
+var down = false;
+document.addEventListener(
+    "keydown",
+    function () {
+        if (down) return;
+        down = true;
+        console.log("SPACES");
+        // your magic code here
+    },
+    false
+);
+
+document.addEventListener(
+    "keyup",
+    function () {
+        down = false;
+    },
+    false
+);
+
+// document.body.addEventListener("keypress", (event) => {
+//     console.log("SPACES");
+// });
