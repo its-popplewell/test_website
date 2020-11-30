c241701008289deefb8f00cdf456ad6db05aeae1
its-popplewell <jpscheffel@gmail.com>
2020-11-30 15:32:23 -0500

    Run multiple times

--- index.js
+++ index.js
@@ -2,46 +2,54 @@ let countHeads = 0;
 let countTails = 0;
 let counter = 0;
 
-console.log('JAK WUZ HERE');
-
-while (countHeads < 6 && countTails < 6) {
-    let choice = Math.floor(Math.random() * 2);
-    if (choice == 0) {
-        countHeads++;
-    } else if (choice == 1) {
-        countTails++;
-    }
-    counter++;
-}
-console.log(`DONE: countHeads: ${countHeads} countTails: ${countTails}`);
-if (counter == 11) {
-    let choice = Math.floor(Math.random() * 2);
-    if (choice == 0) {
-        countHeads++;
-    } else if (choice == 1) {
-        countTails++;
-    }
-    counter++;
-
-    if (countTails == 7 || countHeads == 7) {
-        countTails = countTails == 7 ? 6 : countTails;
-        countHeads = countHeads == 7 ? 6 : countHeads;
-        counter = 11;
-    } else {
-        let choice = Math.floor(Math.random() * 2);
-        if (choice == 0) {
-            countHeads++;
-        } else if (choice == 1) {
-            countTails++;
+const setup = () => {
+    //does 10 simulations
+    for (let i = 0; i < 10; i++) {
+        let specialCase = false;
+        //runs until ...
+        while (countHeads < 6 && countTails < 6) {
+            let choice = Math.floor(Math.random() * 2);
+            if (choice == 0) {
+                countHeads++;
+            } else if (choice == 1) {
+                countTails++;
+            }
+            counter++;
         }
-        counter++;
+        //--------------------------
+        console.log(`DONE: countHeads: ${countHeads} countTails: ${countTails}`);
+        //--------------------------
+        if (counter == 11) {
+            specialCase = true;
+            let choice = Math.floor(Math.random() * 2);
+            if (choice == 0) {
+                countHeads++;
+            } else if (choice == 1) {
+                countTails++;
+            }
+            counter++;
+
+            if (countTails == 7 || countHeads == 7) {
+                countTails = countTails == 7 ? 6 : countTails;
+                countHeads = countHeads == 7 ? 6 : countHeads;
+                counter--;
+            } else {
+                let choice = Math.floor(Math.random() * 2);
+                if (choice == 0) {
+                    countHeads++;
+                } else if (choice == 1) {
+                    countTails++;
+                }
+                counter++;
+            }
+        }
+
+        createDiv('').id(`divider${i}`);
+        createP(specialCase ? 'Special case' : 'Normal Case').parent(`divider${i}`);
+        createP(`Number of Heads: ${countHeads}`).parent(`divider${i}`);
+        createP(`Number of Tails: ${countTails}`).parent(`divider${i}`);
+        createP(`Number of Flips: ${counter}`).parent(`divider${i}`);
     }
+};
 
-    console.log('SPECIAL CASE');
-    document.getElementById('headerThing').innerText = 'SPECIAL CASE';
-}
-
-document.getElementById('numFlips').innerText = counter;
-document.getElementById('numHeads').innerText = countHeads;
-document.getElementById('numTails').innerText = countTails;
-document.getElementById('prob').innerText = 'Calculate yourself.';
+const draw = () => {};
